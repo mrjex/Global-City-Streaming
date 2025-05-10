6790337580f4419563782fcedcd1b509af216c62
mrjex <joel.mattsson@hotmail.se>
2025-05-10 15:28:42 +0200

    Use terraform-docs for automated doc generation for terraform modules

---
+++ terraform/versions.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = ">= 2.0.0"
+    }
+  }
+
+  required_version = ">= 1.11.4"
+}
