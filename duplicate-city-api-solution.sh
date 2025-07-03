f916217ded3659b4f99a8ca3670063568fd69782
mrjex <joel.mattsson@hotmail.se>
2025-07-03 02:53:28 +0200

    Handle edge cases for city temperatures of selected country

--- duplicate-city-api-solution.sh
+++ duplicate-city-api-solution.sh
@@ -3,10 +3,10 @@
 source .env
 
 # Number of unique cities we want to get
-TARGET_UNIQUE_CITIES=10
+TARGET_UNIQUE_CITIES=7
 
 # Extra cities to fetch to account for potential duplicates
-DUPLICATE_BUFFER=4
+DUPLICATE_BUFFER=3
 
 # Total number of cities to request from API
 FETCH_LIMIT=$((TARGET_UNIQUE_CITIES + DUPLICATE_BUFFER))
@@ -14,10 +14,10 @@ FETCH_LIMIT=$((TARGET_UNIQUE_CITIES + DUPLICATE_BUFFER))
 # Country code for the query
 COUNTRY_CODE=DE
 
-# Temporary file to store API response
-API_RESPONSE=$(curl -s -X GET "https://wft-geo-db.p.rapidapi.com/v1/geo/cities?countryIds=${COUNTRY_CODE}&limit=${FETCH_LIMIT}&sort=-population&types=CITY" \
-     -H "X-RapidAPI-Host: wft-geo-db.p.rapidapi.com" \
-     -H "X-RapidAPI-Key: ${GEODB_CITIES_API_KEY}")
 
-# Extract unique cities and take only the first TARGET_UNIQUE_CITIES
-echo "$API_RESPONSE" | jq -r '.data[].city' | sort -u | head -n "$TARGET_UNIQUE_CITIES"
+curl -s -X GET "https://wft-geo-db.p.rapidapi.com/v1/geo/cities?countryIds=${COUNTRY_CODE}&limit=${FETCH_LIMIT}&sort=-population&types=CITY" \
+     -H "X-RapidAPI-Host: wft-geo-db.p.rapidapi.com" \
+     -H "X-RapidAPI-Key: ${GEODB_CITIES_API_KEY}" | jq -r '.data[].city' | awk '!seen[$0]++' | head -n $TARGET_UNIQUE_CITIES
+
+
+# TODO: New Zealand, Hamilton
